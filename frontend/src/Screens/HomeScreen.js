212e49f20f31a089d3eb7151b9f1d90d230eed22
Milo <milo@Milos-MacBook-Air.local>
2021-04-15 22:51:32 -0400

    added navbar, hero, section

---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+import ServicesSection from '../components/ServicesSection'
+const HomeScreen = () => {
+    return (
+        <>
+            <Header />
+            <ServicesSection />
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ frontend/src/Screens/TestimonialsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestimonialsScreen = () => {
+    return (
+        <div>
+            <h1>Testimonials</h1>
+        </div>
+    )
+}
+
+export default TestimonialsScreen
