9278cabfccdb0460e11b62447cd04e598390b72d
Milo <milo@Milos-MacBook-Air.local>
2021-04-22 12:11:36 -0400

    added rating component, finished card styling

--- frontend/src/Screens/TestimonialsScreen.js
+++ frontend/src/Screens/TestimonialsScreen.js
@@ -15,7 +15,7 @@ const TestimonialsScreen = () => {
     //
     return (
         <section id="section-testimonials">
-            <h1 class="h2-text">Testimonials</h1>
+            <h1 class="h2-title text-center mb-5">Testimonials</h1>
             <div class="container">
                 <div class="row">
                     <div class="col pb-5">
@@ -35,9 +35,54 @@ const TestimonialsScreen = () => {
                                 <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
                                 <div class="d-flex">
                                     <div class="w-50">
-                                        <img src={google} class="img-fluid w-25" />
+                                        <a href="" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" /><span style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
                                     </div>
-                                        <a class="btn btn-blue btn-outline-primary" href="#">View on google</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col pb-5">
+                        <div class="card" style={{width: '20rem'}}>
+                            <div class="card-header d-flex p-text">
+                                {/* <div class="col"> */}
+                                    <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
+                                {/* </div> */}
+                                <div class="col testimonial-header-style mt-1">
+                                    <h5 class="fw-light fs-5 blue">Name</h5>
+                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                {/* <h4 class="card-title p-text">Card Title</h4> */}
+                                <h6 class="card-subtitle text-muted p-text">S.T.A.R.S</h6>
+                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
+                                <div class="d-flex">
+                                    <div class="w-50">
+                                        <a href="" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" /><span style={{fontSize: "14px", alignSelf: "center"}}>View on google</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col pb-5">
+                        <div class="card" style={{width: '20rem'}}>
+                            <div class="card-header d-flex p-text">
+                                {/* <div class="col"> */}
+                                    <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
+                                {/* </div> */}
+                                <div class="col testimonial-header-style mt-1">
+                                    <h5 class="fw-light fs-5 blue">Name</h5>
+                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                {/* <h4 class="card-title p-text">Card Title</h4> */}
+                                <h6 class="card-subtitle text-muted p-text">S.T.A.R.S</h6>
+                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
+                                <div class="d-flex">
+                                    <div class="w-50">
+                                        <a href="" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" /><span style={{fontSize: "14px", alignSelf: "center"}}>View on google</span></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
