6201cf38acef9d25f0e97f1988c78b6edd83218a
Milo <milo@Milos-MacBook-Air.local>
2021-04-22 12:20:47 -0400

    added ratings to card

--- frontend/src/Screens/TestimonialsScreen.js
+++ frontend/src/Screens/TestimonialsScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Rating from '../components/Rating'
 import avatar from '../avatar.png'
 import google from '../google.png'
 const TestimonialsScreen = () => {
@@ -30,8 +31,7 @@ const TestimonialsScreen = () => {
                                 </div>
                             </div>
                             <div class="card-body">
-                                {/* <h4 class="card-title p-text">Card Title</h4> */}
-                                <h6 class="card-subtitle text-muted p-text">S.T.A.R.S</h6>
+                                {<Rating />}
                                 <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
                                 <div class="d-flex">
                                     <div class="w-50">
@@ -44,17 +44,14 @@ const TestimonialsScreen = () => {
                     <div class="col pb-5">
                         <div class="card" style={{width: '20rem'}}>
                             <div class="card-header d-flex p-text">
-                                {/* <div class="col"> */}
                                     <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                {/* </div> */}
                                 <div class="col testimonial-header-style mt-1">
                                     <h5 class="fw-light fs-5 blue">Name</h5>
                                     <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
                                 </div>
                             </div>
                             <div class="card-body">
-                                {/* <h4 class="card-title p-text">Card Title</h4> */}
-                                <h6 class="card-subtitle text-muted p-text">S.T.A.R.S</h6>
+                                {<Rating />}
                                 <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
                                 <div class="d-flex">
                                     <div class="w-50">
@@ -67,17 +64,14 @@ const TestimonialsScreen = () => {
                     <div class="col pb-5">
                         <div class="card" style={{width: '20rem'}}>
                             <div class="card-header d-flex p-text">
-                                {/* <div class="col"> */}
                                     <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                {/* </div> */}
                                 <div class="col testimonial-header-style mt-1">
                                     <h5 class="fw-light fs-5 blue">Name</h5>
                                     <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
                                 </div>
                             </div>
                             <div class="card-body">
-                                {/* <h4 class="card-title p-text">Card Title</h4> */}
-                                <h6 class="card-subtitle text-muted p-text">S.T.A.R.S</h6>
+                                {<Rating />}
                                 <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
                                 <div class="d-flex">
                                     <div class="w-50">
