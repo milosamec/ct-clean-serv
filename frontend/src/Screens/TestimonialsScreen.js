bb090e8010ddbf8baa2018c3e65747471baf995f
Milo <milo@Milos-MacBook-Air.local>
2021-04-23 23:41:19 -0400

    added cards and util

--- frontend/src/Screens/TestimonialsScreen.js
+++ frontend/src/Screens/TestimonialsScreen.js
@@ -1,18 +1,81 @@
-import React from 'react'
-import Rating from '../components/Rating'
+import React, {useState, useEffect} from 'react'
+import GoogleReviewCard from '../components/GoogleReviewCard'
+import YelpReviewCard from '../components/YelpReviewCard'
 import avatar from '../avatar.png'
-import google from '../google.png'
 import googlereviews from '../reviews_google.jpeg'
-import yelpreviews from '../reviews_yelp.jpeg'
+import yelpreview from '../reviews_yelp.jpeg'
 import facebookreviews from '../reviews_facebook.jpeg'
 
 
 const TestimonialsScreen = () => {
+    // Google Reviews
+    let [googleReviews, setGoogleReviews] = useState([])
+    let [yelpReviews, setYelpReviews] = useState([])
+    let reviews = [
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+    ]
+
+    let yelpreviews = [
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+        {
+            reviewerName: "John Smith",
+            reviewerAvatar: avatar,
+            reviewerRating: 5,
+            reviewerDescription: "What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it",
+            date: "",
+            link: "https://google.com/",
+        },
+    ]
+
+
+
+    // Useeffect for reviews
+    useEffect(() => {
+        setGoogleReviews(reviews)
+        setYelpReviews(yelpreviews)
+    }, [])
+
 
     // Date Related
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let d = new Date()
     let currentMonth = monthNames[d.getMonth()]
     let currentMonthShort = currentMonth.slice(0, 3)
@@ -21,145 +84,37 @@ const TestimonialsScreen = () => {
     //
     return (
         <section id="section-testimonials" class="pb-5">
+            {/* DISPLAY GOOGLE REVIEWS */}
             <h1 class="h2-title text-center mb-5">Google Reviews</h1>
             <div class="container">
                 <div class="row mx-auto testimonials-align-center">
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {reviews.length && reviews.map((review) => (
+                        <GoogleReviewCard key={review.reviewerName} review={review}/>
+                    ))}
+
                     <div class="row mx-auto mb-5">
                         <div class="col">
                             <button class="btn btn-blue p-3" type="button">Load More Google Reviews</button>
                         </div>
                     </div>
                 </div>
+
+                {/* DISPLAY YELP REVIEWS */}
                 <div class="row">
                     <h1 class="h2-title text-center mb-5">Yelp Reviews</h1>
                 </div>
                 <div class="row mx-auto testimonials-align-center">
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col pb-5">
-                        <div class="card" style={{width: '20rem'}}>
-                            <div class="card-header d-flex p-text">
-                                <img src={avatar} alt="Avatar" class="avatar w-25 mt-1" />
-                                <div class="col testimonial-header-style mt-1 d-flex align-items-start">
-                                    <h5 class="fw-light fs-5 blue">Name</h5>
-                                    <p class="fw-light fs-6">{`${currentMonthShort} ${day}, ${year}`}</p>
-                                </div>
-                            </div>
-                            <div class="card-body testimonials-align-left">
-                                {<Rating />}
-                                <p class="card-text p-text">What can I say these girls are professional, friendly, and thorough. I have absolutely no concerns about leaving them here while I am out. It is such a great feeling to walk around my home after they are gone and see how beautifully they have left it</p>
-                                <div class="d-flex">
-                                    <div class="w-100">
-                                        <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on google</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {yelpreviews.length && yelpreviews.map((review) => (
+                        <YelpReviewCard key={review.reviewerName} review={review} />
+                    ))}
                     <div class="row mx-auto mb-5">
                         <div class="col">
-                            <button class="btn btn-blue" type="button">Load More Yelp Reviews</button>
+                            <button class="btn btn-blue p-3" type="button">Load More Yelp Reviews</button>
                         </div>
                     </div>
                 </div>
+
+                {/* LAST SECTION */}
                 <div class="row mx-auto">
                     <div class="col-sm-12 text-center">
                         <h2 id="hero-title" className="h2-title p-3">If you are happy with our service, please take a minute to write a review.</h2>
@@ -170,7 +125,7 @@ const TestimonialsScreen = () => {
                         <a href="#" alt="Google Reviews Image"><img className="img img-fluid" src={googlereviews}></img></a>
                     </div>
                     <div class="col-sm-4 pb-3">
-                        <a href="#" alt="Yelp Reviews Image"><img className="img img-fluid" src={yelpreviews}></img></a>
+                        <a href="#" alt="Yelp Reviews Image"><img className="img img-fluid" src={yelpreview}></img></a>
                     </div>
                     <div class="col-sm-4 pb-3">
                         <a href="#" alt="Facebook Reviews Image"><img className="img img-fluid" src={facebookreviews}></img></a>
---
+++ frontend/src/components/GoogleReviewCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Rating from '../components/Rating'
+import DateUtil from '../utils/DateUtil'
+import google from '../google.png'
+import avatar from '../avatar.png'
+
+const GoogleReviewCard = ({review}) => {
+    const {date, link, reviewerAvatar, reviewerDescription, reviewerName, reviewerRating} = review
+    // Review Posted Date //
+    let d = new Date()
+    return (
+        <>
+            <div class="col pb-5">
+                <div class="card" style={{width: '20rem'}}>
+                    <div class="card-header d-flex p-text">
+                        <img src={reviewerAvatar} alt="Avatar" class="avatar w-25 mt-1" />
+                        <div class="col testimonial-header-style mt-1 d-flex align-items-start">
+                            <h5 class="fw-light fs-5 blue">{reviewerName}</h5>
+                            <p class="fw-light fs-6">{<DateUtil date={d}/>}</p>
+                        </div>
+                    </div>
+                    <div class="card-body testimonials-align-left">
+                        {<Rating />}
+                        <p class="card-text p-text">{reviewerDescription}</p>
+                        <div class="d-flex">
+                            <div class="w-100">
+                                <a href="#" class="d-flex links about-team-blue fs-6 text" style={{height: "25px", fontSize: "16px"}}><img src={google} class="img-fluid" style={{maxWidth: "10%"}} /><span className="mx-1" style={{fontSize: "14px", alignSelf: "center" }}>View on Google</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GoogleReviewCard
